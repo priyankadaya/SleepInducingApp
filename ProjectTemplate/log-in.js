3929334cef630283da91ed876ba43138b7b19520
Connor Holmes <you@example.com>
2021-02-11 15:35:36 -0700

    Added function to change brightness

--- ProjectTemplate/log-in.js
+++ ProjectTemplate/log-in.js
@@ -1,4 +1,22 @@
 // Created by Connor Holmes
+
+// Used for determing background brightness
+// Day = light
+// Night = dark
+function getBrightness() {
+    // Returns hours as an int between 0 and 23
+    function getTimeOfDay() {
+        var d = new Date();
+        return d.getHours();
+    }
+    if (getTimeOfDay() > 5 || getTimeOfDay() < 19) {
+        document.getElementsByName('body').style.filter = "brightness(1.75)";
+    } else {
+        document.getElementsByName('body').style.filter = "brightness(50%)";
+    }
+}
+
+
 function attemptLogin() {
     const url = 'ProjectServices.asmx/LogOn'
     const data = {
@@ -11,3 +29,5 @@ function attemptLogin() {
         data: data
     }).then(data => console.log(data)).catch(err => alert('Error\n' + err));
 }
+
+getBrightness();
