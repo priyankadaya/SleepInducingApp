3606c6f77b13c62786554ddf7e3ea6c05ed3854e
Connor Holmes <you@example.com>
2021-02-18 15:20:05 -0700

    updated code

--- ProjectTemplate/log-in.js
+++ ProjectTemplate/log-in.js
@@ -4,6 +4,11 @@
 // Day = light
 // Night = dark
 
+// Called after successful log-in
+function navigateToSite() {
+
+}
+
 function getBrightness() {
     // Returns hours as an int between 0 and 23
     function getTimeOfDay() {
@@ -21,15 +26,32 @@ function getBrightness() {
 
 function attemptLogin() {
     const url = 'ProjectServices.asmx/LogOn'
+    var username = document.getElementById('username-entry').value;
+    var password = document.getElementById('password-entry').value;
     var data = {
-        username: document.getElementById('username-entry').value,
-        password: document.getElementById('password-entry').value
-    }
-    axios({
-        method: 'post',
+            username: document.getElementById('username-entry').value,
+            password: document.getElementById('password-entry').value
+        }
+        // axios({
+        //     method: 'post',
+        //     url: url,
+        //     data: JSON.stringify(data)
+        // }).then(data => console.log(data) && alert('Log-in Successful') && navigateToSite()).catch(err => alert('Error\n' + err));
+    $.ajax({
+        type: "POST",
         url: url,
-        data: JSON.stringify(data)
-    }).then(data => console.log(data) && alert('Log-in Successful')).catch(err => alert('Error\n' + err));
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: data,
+        success: function(msg) {
+            alert('log-in succesful')
+        },
+        error: function(e) {
+            alert('Something went seriously wrong.');
+        }
+    });
+
 }
+
 
 getBrightness();
