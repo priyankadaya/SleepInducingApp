9889f744c7b1e7037c44821d5e160d2bdbc969f0
Justin <Justin@JUSTINSPC>
2021-02-20 20:56:32 -0700

    Added navbar with check admin features and synced nav links together

---
+++ ProjectTemplate/admin_navbar.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    CheckAdmin();
+});
+
+
+function CheckAdmin() {
+    var webMethod = "ProjectServices.asmx/CheckAdmin";
+
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var responseFromServer = msg.d;
+            var x = document.getElementById("users");
+
+            if (responseFromServer == false) {
+                $(x.style.display = "none");
+                
+            }
+        },
+        error: function (e) {
+            alert('Something is wrong.');
+        }
+    });
+}
