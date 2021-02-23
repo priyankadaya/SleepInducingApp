5b3d2a9d66f3e1b682d9db5caeee19595f021061
jdarr1 <61485710+jdarr1@users.noreply.github.com>
2021-02-23 01:32:41 -0700

    put the class in the right place this time

---
+++ ProjectTemplate/Rec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate
+{
+    public class Rec
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set;  }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public string DateSubmitted { get; set; }
+
+    }
+}
